be5b6bda0156449de0818597cfb325a6e88c5295
c0mpl9x <marcos10ev@gmail.com>
2024-12-03 19:08:24 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -45,12 +45,12 @@ function toggleScanlines() {
 
 // Helper for toggling CRT color theme
 function greenTheme() {
-    $("#greenTheme").prop("checked", false);
+    $("#greenTheme").prop("checked", true);
     $("body").addClass("green");
     createCookie('greenTheme',1);
 }
 function amberTheme() {
-    $("#greenTheme").prop("checked", true);
+    $("#greenTheme").prop("checked", false);
     $("body").removeClass("green");
     createCookie('greenTheme',0);
 }
